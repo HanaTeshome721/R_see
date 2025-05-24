50ea6f2c0f8353c966a9218f9837828ee1e61429
Hana-Teshome <hanatm1621@gmail.com>
2025-05-24 14:19:59 +0300

    feat: add initial project structure with components and styles

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { use, useEffect, useState } from "react"
+
+const Card=({title})=>{
+  const [count,setCount]=useState(0);
+  const [ hasliked,sethasliked]=useState(false);
+
+  useEffect(() => {
+    console.log(`The card with title "${title}" has been liked:${hasliked}.`);
+    
+  },[hasliked])
+
+  useEffect(() => {
+    console.log(`The card times.`);
+  },[])
+
+   
+   return(
+    < div className="card" onClick={() => setCount((prev) => prev + 1)}>
+      <h4>{title}<br/>{count || null}</h4>
+      <button onClick={() => sethasliked(!hasliked)}>
+       { hasliked ? "Unlike" : "Like"}
+      </button>
+    </div>
+   )
+}
+
+const App=() =>{
+   
+ return (
+  <>
+ 
+  <h2>welcom my baby</h2>
+   <Card title="Star war"/>
+  <Card title="Avater"/>
+  <Card title="The lion King"/>
+  </>
+ )
+
+}
+
+export default App
